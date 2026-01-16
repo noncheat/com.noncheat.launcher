5767bab023fd0f300afa9b4d1ddc3ef4cbb597cd
quyenvsp <quyenvsp@gmail.com>
2026-01-17 00:18:33 +0700

    chore: update new version prevent call QuickTiles because accessibility slow

--- app/src/main/java/com/noncheat/launcher/Hook.java
+++ app/src/main/java/com/noncheat/launcher/Hook.java
@@ -4,50 +4,72 @@ import static de.robv.android.xposed.XposedBridge.log;
 
 import android.app.AndroidAppHelper;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
-
-import java.lang.reflect.Member;
+import android.view.ViewConfiguration;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
     private static final String TAG = "NoncheatLauncher ";
+    protected final String packageName = "com.android.systemui";
+    protected final String packageNameLauncher = "com.google.android.apps.nexuslauncher";
+    private Long lastClick = null;
+    private PowerManager mPowerManager = null;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        Class<?> classWorkspaceTouchListener = XposedHelpers.findClass("com.android.launcher3.touch.WorkspaceTouchListener", loadPackageParam.classLoader);
-        Class<?> classLauncher = XposedHelpers.findClass("com.android.launcher3.Launcher", loadPackageParam.classLoader);
-        Class<?> classWorkspace = XposedHelpers.findClass("com.android.launcher3.Workspace", loadPackageParam.classLoader);
-        XposedHelpers.findAndHookConstructor(
-                classWorkspaceTouchListener,
-                classLauncher,
-                classWorkspace,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        Class<?> classGestureDetector$SimpleOnGestureListener = XposedHelpers.findClass("android.view.GestureDetector$SimpleOnGestureListener", loadPackageParam.classLoader);
-                        XposedHelpers.findAndHookMethod(
-                                classGestureDetector$SimpleOnGestureListener,
-                                "onDoubleTap",
-                                MotionEvent.class,
-                                new XC_MethodHook() {
-                                    @Override
-                                    protected void beforeHookedMethod(MethodHookParam param) {
-                                        Intent intent = new Intent();
-                                        intent.setComponent(new ComponentName("com.asdoi.quicktiles", "com.asdoi.quicksettings.MainActivity"));
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        AndroidAppHelper.currentApplication().startActivity(intent);
-                                    }
-                                }
-                        );
+        if (loadPackageParam.packageName.equals(packageName)) {
+            Class<?> classNotificationPanelViewController = XposedHelpers.findClass("com.android.systemui.shade.NotificationPanelViewController", loadPackageParam.classLoader);
+            XposedHelpers.findAndHookMethod(classNotificationPanelViewController, "onEmptySpaceClick", float.class, float.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) {
+                    if (mPowerManager == null) {
+                        Context mContext = AndroidAppHelper.currentApplication().getApplicationContext();
+                        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+                        log(TAG + "set power manager " + (mPowerManager != null ? "[success]" : "[fail]"));
                     }
+                    onClick();
                 }
-        );
+            });
+        } else if (loadPackageParam.packageName.equals(packageNameLauncher)) {
+            Class<?> classWorkspace = XposedHelpers.findClass("com.android.launcher3.Workspace", loadPackageParam.classLoader);
+            XposedHelpers.findAndHookConstructor(classWorkspace, Context.class, AttributeSet.class, int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    Class<?> classGestureDetector$SimpleOnGestureListener = XposedHelpers.findClass("android.view.GestureDetector$SimpleOnGestureListener", loadPackageParam.classLoader);
+                    XposedHelpers.findAndHookMethod(classGestureDetector$SimpleOnGestureListener, "onDoubleTap", MotionEvent.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) {
+                            Intent intent = new Intent();
+                            intent.setComponent(new ComponentName("com.noncheat.lockscreen", "com.noncheat.lockscreen.MainActivity"));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            AndroidAppHelper.currentApplication().startActivity(intent);
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void onClick() {
+        Long currentTime = System.currentTimeMillis();
+        if (lastClick != null && currentTime - lastClick <= ViewConfiguration.getDoubleTapTimeout()) {
+            if (mPowerManager == null) {
+                log(TAG + "no power manager");
+                return;
+            }
+            XposedHelpers.callMethod(mPowerManager, "goToSleep", SystemClock.uptimeMillis());
+            lastClick = null;
+        } else {
+            lastClick = currentTime;
+        }
     }
 }
